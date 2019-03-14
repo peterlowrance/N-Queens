0f09e1d5605d85f3851d38c79199c7ca59300728
peterlowrance <peter.lowrance@gmail.com>
2019-03-14 15:46:45 -0400

    added jUnit tests

--- src/Board.java
+++ src/Board.java
@@ -20,15 +20,16 @@ public class Board {
 		board[x][y] = q;
 	}
 
-/**
- * Returns true if there are no collisions
- * @return true if the board satisfies the constraints
- */
+	/**
+	 * Returns true if there are no collisions
+	 * 
+	 * @return true if the board satisfies the constraints
+	 */
 	public boolean isDone() {
-		for(int i=0; i<size; i++){
-			for(int j=0; j<size; j++){
-				if(board[i][j]){
-					if(!isOpen(i,j)){
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (board[i][j]) {
+					if (!isOpen(i, j)) {
 						return false;
 					}
 				}
@@ -37,21 +38,38 @@ public class Board {
 		return true;
 	}
 
-private boolean isOpen(int i, int j) {
-	// TODO Auto-generated method stub
-	return false;
-}
+	public boolean isOpen(int i, int j) {
+		for (int k = 0; k < size; k++) {
+			if (board[i][k] && k != j) {
+				return false;
+			}
+		}
+		for (int k = 0; k < size; k++) {
+			if (board[k][j] && k != i) {
+				return false;
+			}
+		}
+		// TODO check diagonals
+		/*
+		 * for(int k=0; i<size; i++) { if(board[k-i][k-j] && k != j && k != i) { return
+		 * false; } }
+		 */
+		return true;
+	}
 
-/**
- * Returns the number of collisions. Returns 0 if there are no collisions
- * @return the number of collisions
- */
-	public int isDoneNumCollisions(){
+	/**
+	 * Returns the number of collisions. Returns 0 if there are no collisions
+	 * 
+	 * @return the number of collisions
+	 */
+	public int isDoneNumCollisions() {
 		int numCollisions = 0;
-		for(int i=0; i<size; i++){
-			for(int j=0; j<size; j++){
-				if(board[i][j]){
-					if(!isOpen(i,j)){
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (board[i][j]) {
+					// System.out.println(i + " " + j + " " + isOpen(i,j));
+					if (!isOpen(i, j)) {
+						// System.out.println("collide");
 						numCollisions++;
 					}
 				}
--- src/BoardTest.java
+++ src/BoardTest.java
@@ -1,9 +1,52 @@
+import org.junit.Assert;
+import org.junit.Test;
 
 public class BoardTest {
-	public static void main(String[] args) {
+	@Test 
+	public void createEmptyBoard() {
 		Board board = new Board(2);
-		board.set(0,0, true);
-		System.out.println(board.isDone());
-		System.out.println("Finished");
+		Assert.assertFalse(board.get(0, 0));
+	}
+	@Test 
+	public void setBoard() {
+		Board board = new Board(2);
+		board.set(0,0,true);
+		Assert.assertTrue(board.get(0, 0));
+	}
+	@Test 
+	public void largeBoard() {
+		Board board = new Board(20);
+		board.set(10,10,true);
+		Assert.assertTrue(board.get(10, 10));
+	}
+	@Test 
+	public void checkOpenHorizontal() {
+		Board board = new Board(2);
+		board.set(0,0,true);
+		Assert.assertFalse(board.isOpen(1,0));
+	}
+	@Test 
+	public void checkOpenVertical() {
+		Board board = new Board(2);
+		board.set(0,0,true);
+		Assert.assertFalse(board.isOpen(0,1));
+	}
+	@Test 
+	public void checkOpenDiagonal() {
+		Board board = new Board(2);
+		board.set(0,0,true);
+		Assert.assertFalse(board.isOpen(1,1));
+	}
+	@Test 
+	public void checkOpenTrue() {
+		Board board = new Board(3);
+		board.set(0,0,true);
+		Assert.assertTrue(board.isOpen(2,1));
+	}
+	@Test 
+	public void checkIsDoneTrue() {
+		Board board = new Board(3);
+		board.set(0,0,true);
+		Assert.assertTrue(board.isDone());
 	}
 }
