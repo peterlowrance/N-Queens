b854c0cdccdf22be632bb366df674ed2fd105cc4
peterlowrance <peter.lowrance@gmail.com>
2019-03-18 23:26:49 -0400

    more gen alg

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -1,11 +1,14 @@
+import java.util.Random;
 
 public class GeneticAlgorithm {
 
 	private int boardsPerGen;
 	private int n;
 	private Board[] boards;
+	static Random rand;
 
 	public static void main(String[] args) {
+		rand = new Random();
 		GeneticAlgorithm thisAlgorithm = new GeneticAlgorithm(4, 4);
 		thisAlgorithm.solution();
 	}
@@ -29,6 +32,40 @@ public class GeneticAlgorithm {
 			System.out.println(":" + b.getFitness());
 		}
 		return null;
+	}
+	
+	public Board[] spliceDoulbe(Board[] boards) {
+		Board[] splicedBoards = new Board[boardsPerGen];
+		for(int i=0; i< boards.length; i+= 2) {
+			int splicePoint = rand.nextInt(n);
+			Board newBoard1 = new Board(n, false);
+			for(int j=0; i<n; j++) {
+				if(j < splicePoint) {
+					setCol(newBoard1, boards[i], j);
+				}
+				else {
+					setCol(newBoard1, boards[i+1], j);
+				}
+			}
+			splicedBoards[i] = newBoard1;
+			Board newBoard2 = new Board(n, false);
+			for(int j=0; i<n; j++) {
+				if(j < splicePoint) {
+					setCol(newBoard2, boards[i+1], j);
+				}
+				else {
+					setCol(newBoard2, boards[i], j);
+				}
+			}
+			splicedBoards[i+1] = newBoard2;
+		}
+		return splicedBoards;
+	}
+	
+	public void setCol(Board getCol, Board setCol, int col) {
+		for(int i=0; i<n; i++) {
+			setCol.set(i, col, getCol.get(i, col));
+		}
 	}
 
 	public Board[] bestHalf(Board[] boards) {
